2abd0def32fe38d518883243473769a7997230c9
DifenYedidya <difen.yedidya@gmail.com>
2022-11-23 22:35:05 +0700

    crud

---
+++ readme.md
@@ -0,0 +1,78 @@
+# Learning Basic React
+
+## Requirement
+
+- NodeJS v16++
+  > I use Volta.sh for install NodeJS which is supporting multiple version of NodeJS and use pnpm.io for dependency management
+- Visual Studio Code
+- Figma
+- Git
+  > If you use Windows for developement OS, i recommend you to use Windows WSL2 with Windows terminal and zsh shell
+- Browser (Firefox, Google Chrome, Microsoft Edge, Brave)
+  > I use Firefox Developer Edition + React Dev Tools Extension
+- Terminal (Bash, Zsh, Fish)
+  > I use Fish Shell, another recommendation is zsh shell
+
+## How to run this Project
+
+- Install dependencies
+
+```shell
+$ pnpm install
+```
+
+- Run Dev Server
+
+```shell
+$ pnpm run dev
+```
+
+- Run Test
+
+```shell
+$ pnpm run test
+```
+
+## What we gonna learn
+
+- Typescript over Javascript
+- Git
+
+- What is ReactJS
+
+  - Basic Theory
+  - Creating ReactJS project
+
+- Component
+- State & Props
+- TailwindCSS
+- React Hooks
+- Form
+- Data Fetching
+
+  - Basic RESTFull API
+  - Authentication & Authorization
+  - Managing Network State
+
+- Error Handling
+- Testing
+  - Unit Test + Component Test
+  - Integration Test
+  - End to End (E2E) Testing
+
+## References
+
+- [Volta](https://volta.sh)
+- [PNPM](https://pnpm.io)
+- [ViteJS](https://vitejs.dev)
+- [Typescript](https://www.typescriptlang.org/docs)
+- [ReactJS Roadmap](https://roadmap.sh/react)
+- [ReactJS Documentation](https://beta.reactjs.org)
+- [Axios](https://axios-http.com)
+- [React Query](https://tanstack.com/query/v4/docs/adapters/react-query)
+- [React Hook Form](https://react-hook-form.com)
+- [Zod](https://zod.dev)
+- [React Hook Form Resolvers](https://github.com/react-hook-form/resolvers)
+- [TailwindCSS](https://tailwindcss.com)
+- [Vitest](https://vitest.dev)
+- [Vercel](https://vercel.com)
---
+++ src/App.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import Card from "./components/Card";
+
+type Todo = {
+  id: number;
+  title: string;
+  isDone: boolean;
+  createdAt: Date;
+};
+
+const App: React.FC = () => {
+  const [title, setTitle] = React.useState("");
+  const [todos, setTodos] = React.useState<Todo[]>([
+    {
+      id: 1,
+      title: "First todo",
+      isDone: false,
+      createdAt: new Date(),
+    },
+  ]);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    // prevent to redirect to another page
+    e.preventDefault();
+    // add todos state
+    setTodos((prev) => {
+      return [
+        ...prev,
+        { id: Date.now(), title: title, isDone: false, createdAt: new Date() },
+      ];
+    });
+
+    // clear input
+    setTitle("");
+  };
+
+  const handleToggle = (id: number) => {
+    setTodos(
+      todos.map((todo) => {
+        if (id === todo.id) {
+          todo.isDone = !todo.isDone;
+        }
+        return todo;
+      })
+    );
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div className="w-full min-h-screen grid place-content-center">
+      <div className="flex flex-col w-80 text-sm gap-4">
+        <form
+          className="flex bg-white py-2 px-4 gap-4"
+          onSubmit={(e) => handleSubmit(e)}
+        >
+          <input
+            type="text"
+            className="flex-1 bg-transparent outline-none"
+            placeholder="Add new todo"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="bg-slate-400 py-2 px-4 text-white rounded-sm text-xs cursor-pointer"
+          >
+            add
+          </button>
+        </form>
+        <ul className="flex flex-col gap-2 h-80 overflow-auto">
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              <Card
+                handleToggle={handleToggle}
+                handleDelete={handleDelete}
+                {...todo}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default App;
